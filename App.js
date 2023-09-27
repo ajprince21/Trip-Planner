db3c91624f39a8fac3f34a9faa53546f70d5b536
Ajay Raj Tiwari <ajprince0607@gmail.com>
2023-09-27 19:03:10 +0530

    initial

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Splash from './src/screens/Splash'
+
+const App = () => {
+  return (
+    
+     <Splash/>
+   
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ src/screens/Splash.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+const Splash = () => {
+    
+
+    useEffect(() => {
+        setTimeout(() => {
+
+        }, 3000);
+    }, [])
+    return (
+        <View style={styles.container}>
+            <Image
+                source={require('../images/tripPlannerlogo.png')}
+                style={styles.logo}
+            />
+            <Text style={styles.text}>Travel with Ease, Plan with Pleasure</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#353935',
+    },
+    text: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        color:'#F0F0F0',
+        textAlign:'center',
+        marginTop:10,
+        fontStyle: 'italic',
+    },
+    logo: {
+        width: 150,
+        height: 150,
+        resizeMode: 'contain',
+        marginTop: 20,
+    },
+});
+
+export default Splash;
